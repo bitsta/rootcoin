5d5ed6bc78e775968017aa6f599465256d09ce0e
bitsta <bitsta@continuumtalk.com>
2016-02-22 18:58:39 +0100

    add non-string parameter
    add non-string parameter conversion for method "importstealthaddress"

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1190,6 +1190,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "signrawtransaction"     && n > 1) ConvertTo<Array>(params[1], true);
     if (strMethod == "signrawtransaction"     && n > 2) ConvertTo<Array>(params[2], true);
     if (strMethod == "sendtostealthaddress"   && n > 1) ConvertTo<double>(params[1]);
+    if (strMethod == "importstealthaddress"   && n > 2) ConvertTo<bool>(params[1]);
 
     return params;
 }
