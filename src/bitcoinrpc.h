625d69695963d618533ea0a7c849bffa1cfd47bf
bitsta <bitsta@continuumtalk.com>
2016-02-26 21:27:47 +0100

    add stealthaddr methods to rpc header

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -205,4 +205,10 @@ extern json_spirit::Value getblock(const json_spirit::Array& params, bool fHelp)
 extern json_spirit::Value getblockbynumber(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getcheckpoint(const json_spirit::Array& params, bool fHelp);
 
+//stealthaddr for anontx
+extern json_spirit::Value getnewstealthaddress(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value liststealthaddresses(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value importstealthaddress(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value sendtostealthaddress(const json_spirit::Array& params, bool fHelp);
+
 #endif
