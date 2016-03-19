7b0a66265db0d0c0af360402bc6e8095ef2f26c2
bitsta <bitsta@users.noreply.github.com>
2016-03-19 12:19:08 +0100

    update class CScriptVisitor
    extend class CKeyStoreIsMineVisitor

--- src/script.cpp
+++ src/script.cpp
@@ -1467,6 +1467,7 @@ public:
     bool operator()(const CNoDestination &dest) const { return false; }
     bool operator()(const CKeyID &keyID) const { return keystore->HaveKey(keyID); }
     bool operator()(const CScriptID &scriptID) const { return keystore->HaveCScript(scriptID); }
+    bool operator()(const CStealthAddress &stxAddr) const { return stxAddr.scan_secret.size() == ec_secret_size; } 
 };
 
 bool IsMine(const CKeyStore &keystore, const CTxDestination &dest)
@@ -1866,6 +1867,14 @@ public:
         script->clear();
         *script << OP_HASH160 << scriptID << OP_EQUAL;
         return true;
+    }
+    
+ 
+    bool operator()(const CStealthAddress &stxAddr) const {
+        script->clear();
+        //*script << OP_HASH160 << scriptID << OP_EQUAL;
+        printf("TODO\n");
+        return false;
     }
 };
 
